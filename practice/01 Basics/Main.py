c22dfa3824d567b64b7ab981b03914b77c5048ed
Herreinstein Roman <63233856+GRom32Bit@users.noreply.github.com>
2024-09-23 15:08:58 +0500

    Add files via upload

--- practice/01 Basics/Main.py
+++ practice/01 Basics/Main.py
@@ -88,7 +88,7 @@ test_matrices(B1,B2)
 print("Task 3 finished!")
 
 #Задание 4.
-url = './datasets/part1/CBF_TRAIN.txt'
+url = './datasets/part1/CBF_TRAIN.txt' #Датасет содержит 30 искусственно созданных временных рядов.
 data = read_ts(url)
 ts_set = data.iloc[:, 1:]
 labels = data.iloc[:, 0]
@@ -135,12 +135,13 @@ for i in n_clusters:
 
     silhouette_DTW = silhouette_score(ts_set, DLabs)
     silhouette_EU = silhouette_score(ts_set, ELabs)
-    print("For DTW\t n_clusters =", i, "the average silhouette_score is :", silhouette_DTW)
-    print("For EU\t n_clusters =", i, "the average silhouette_score is :", silhouette_EU)
+    print("For DTW\t n_clusters =", i, "average silhouette_score is :", silhouette_DTW)
+    print("For EU\t n_clusters =", i, "average silhouette_score is :", silhouette_EU)
     print()
     #Чем ближе к 1, тем больше пересечение силуэтов. Поэтому с существенным отрывом лучше эвклидова метрика. По-хорошему чем больше кластеров, тем хуже результаты.
-    #Эвклидово расстояние больше подходит для временных рядов, поскольку нужно абсолютное значение, а не относительное.
-
+    #Эвклидово расстояние применяется только для рядов одной длины.
+    #DTW подходит для временных рядов, имеющих похожие подпоследовательности (смещённые по времени или сжатые/растянутые). В нашем случае данную подпоследовательность выделить тяжело.
+print("Task 4 finished!")
 
 #Задание 5. Готово.
 def const_find(a):
@@ -161,6 +162,7 @@ def norm_ED_distance(a,b):
     return math.sqrt(abs(2*n*(1-D)))
 DistOwn5=norm_ED_distance(VR1,VR2)
 test_distances(DistOwn5,euclidean_distance(z_normalize(VR1),z_normalize(VR2)))
+print("Task 5 finished!")
 
 #Задание 6.
 url1 = './datasets/part2/chf10.csv'
@@ -168,17 +170,35 @@ ts1 = read_ts(url1)
 url2 = './datasets/part2/chf11.csv'
 ts2 = read_ts(url2)
 
+
 ts_set = np.concatenate((ts1, ts2), axis=1).T
 plot_ts(ts_set)
-
 m = 125
-subs_set1 = sliding_window(ts_set[0], m, m-1)
-subs_set2 = sliding_window(ts_set[1], m, m-1)
+subs_set1 = sliding_window(ts_set[0], m, m - 1)
+subs_set2 = sliding_window(ts_set[1], m, m - 1)
 
 subs_set = np.concatenate((subs_set1[0:15], subs_set2[0:15]))
-labels = np.array([0]*subs_set1[0:15].shape[0] + [1]*subs_set2[0:15].shape[0])
+labels = np.array([0] * subs_set1[0:15].shape[0] + [1] * subs_set2[0:15].shape[0])
 
+#Дополнительная ссылка на методы иерархической кластеризации и разницу между ними.
+#https://stats.stackexchange.com/questions/195446/choosing-the-right-linkage-method-for-hierarchical-clustering
 
+#Рассчитываем расстояния и визуализируем результаты для классической евклидовой метрики.
+distance_matrix_euclidean = PairwiseDistance(metric='euclidean', is_normalize=False).calculate(subs_set)
+clustering_euclidean = TimeSeriesHierarchicalClustering(n_clusters=2, method='complete').fit(distance_matrix_euclidean)
+clustering_euclidean.plot_dendrogram(subs_set, labels, title='Dendrogram for Euclidean Distance')
+
+#Рассчитываем расстояния и визуализируем результаты для нормализованной евклидовой метрики.
+distance_matrix_norm_euclidean = PairwiseDistance(metric='euclidean', is_normalize=True).calculate(subs_set)
+clustering_norm_euclidean = TimeSeriesHierarchicalClustering(n_clusters=2, method='complete').fit(distance_matrix_norm_euclidean)
+clustering_norm_euclidean.plot_dendrogram(subs_set, labels, title='Dendrogram for Normalized Euclidean Distance')
+
+silhouette_eucl = silhouette_score(subs_set,clustering_euclidean.fit_predict(distance_matrix_euclidean))
+silhouette_norm_eucl = silhouette_score(subs_set,clustering_norm_euclidean.fit_predict(distance_matrix_norm_euclidean))
+
+print('eucl \t silhouette score:\t\t{0}'.format(silhouette_eucl))
+print('eucl norm silhouette score:\t\t{0}'.format(silhouette_norm_eucl))
+print("Task 6 finished!")
 #Задание 7.
 
 
